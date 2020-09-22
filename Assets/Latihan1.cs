904d9991c66805b4cfd27d21b5b5d86a33834485
gagahdragon <gagahsetiawan@gmail.com>
2020-09-22 21:47:54 +0700

    assignment no 2

---
+++ Assets/Latihan1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Dicoding!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Latihan3.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /// <summary>
+    /// Merupakan fungsi penjumlahan dari 2 input angka
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    int Jumlah(int a, int b)
+    {
+        return a + b;
+    }
+    void Start()
+    {
+        int c = Jumlah(60, 40);
+        Debug.Log("Hasil jumlah a dan b adalah " + c);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
