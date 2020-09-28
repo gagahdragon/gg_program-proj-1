ea54c34392e4c391db7daf9a26d358f1609b3c72
gagahdragon <gagahsetiawan@gmail.com>
2020-09-28 22:34:19 +0700

    assignemtn no 3

---
+++ Assets/Latihan5/Manusia.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manusia : MonoBehaviour
+{
+    public void Makan()
+    {
+        Debug.Log("Perlu Makan");
+    }
+    public void Tidur()
+    {
+        Debug.Log("Perlu Tidur");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
